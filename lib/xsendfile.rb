365b4d08a7440860bb3f7313d36d49039a7b7803
pjkelly <pj@crushlovely.com>
2010-10-27 14:00:50 -0700

    Pulling the most current version (0.12) from GitHub instead of the old URL which doesn't exist anymore.  Updated configuration options.  XSendFileAllowAbove was removed in version 0.10.

--- lib/xsendfile.rb
+++ lib/xsendfile.rb
@@ -1,37 +1,36 @@
 module Xsendfile
 
   # Define options for this plugin via the <tt>configure</tt> method
-  # in your application manifest:
+  # in your application manifest (none are needed by default):
   #
-  #   configure(:xsendfile => {:x_send_file_allow_above => 'off'})
+  #   configure(:xsendfile => {:x_send_file_path => '/some/absolute/path'})
   #
-  # Then include the plugin and call the recipe(s) you need:
+  # Then call the recipe:
   #
-  #  plugin :xsendfile
   #  recipe :xsendfile
   def xsendfile(options = {})
-    
     package 'apache2-threaded-dev', :ensure => :installed
-    
+
     exec 'install_xsendfile',
       :cwd => '/tmp',
       :command => [
-        'wget http://tn123.ath.cx/mod_xsendfile/mod_xsendfile.c',
+        'wget http://github.com/nmaier/mod_xsendfile/raw/0.12/mod_xsendfile.c',
         'apxs2 -ci mod_xsendfile.c'
       ].join(' && '),
       :require => package('apache2-threaded-dev'),
       :before => service('apache2'),
       :creates => '/usr/lib/apache2/modules/mod_xsendfile.so'
-    
-      file '/etc/apache2/mods-available/xsendfile.conf',
-        :alias => 'xsendfile_conf',
-        :content => """
-XSendFile #{ options[:x_send_file] || 'on'}
-XSendFileAllowAbove #{ options[:x_send_file_allow_above] || 'on'}
-#{options[:extra] || ''}
-        """,
-        :mode => '644',
-        :notify => service('apache2')  
+
+    conf = ["XSendFile #{ options[:x_send_file] || 'on'}"]
+    conf << "XSendFileIgnoreEtag #{options[:x_send_file_ignore_etag] || 'off'}"
+    conf << "XSendFileIgnoreLastModified #{options[:x_send_file_ignore_last_modified] || 'off'}"
+    conf << "XSendFilePath #{options[:x_send_file_path]}" if options[:x_send_file_path]
+
+    file '/etc/apache2/mods-available/xsendfile.conf',
+      :alias => 'xsendfile_conf',
+      :content => conf.join("\n"),
+      :mode => '644',
+      :notify => service('apache2')
 
     file '/etc/apache2/mods-available/xsendfile.load',
       :alias => 'load_xsendfile',
@@ -41,7 +40,6 @@ XSendFileAllowAbove #{ options[:x_send_file_allow_above] || 'on'}
       :notify => service('apache2')
 
    a2enmod 'xsendfile', :require => file('load_xsendfile')
-
   end
-  
+
 end
